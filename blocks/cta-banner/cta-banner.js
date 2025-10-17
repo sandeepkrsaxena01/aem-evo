736131552037a5b1039972d5106b075daf4ed69a
sandeepkrsaxena01 <sandeep.kumar01@gspann.com>
2025-10-17 09:59:03 +0530

    ui tdg

--- blocks/cta-banner/cta-banner.js
+++ blocks/cta-banner/cta-banner.js
@@ -1,3 +1,6 @@
 export default function decorate() {
-// console.log("block", block)
+// console.log("block", block.querySelector(':scope > div:nth-child(1)'))
+// const titleEl = block.querySelector(':scope > div:nth-child(1)');
+// const title = titleEl ? titleEl.textContent.trim() : '';
+// console.log('title', title)
 }
